dcfbb68285f86e0baa8e37d30303798ebfddbd6f
AlexanderStout <132599440+AlexanderStout@users.noreply.github.com>
2023-05-21 18:08:19 +0400

    zsh k8s autocomplete

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#autocomplete for k8s
+source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
+echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
