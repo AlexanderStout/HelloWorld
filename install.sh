6ea3230923c9dc7d162c2ecbbf396a65d952e553
AlexanderStout <132599440+AlexanderStout@users.noreply.github.com>
2023-06-13 12:32:08 +0400

    Terraform

--- install.sh
+++ install.sh
@@ -18,3 +18,7 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
+
+# Terraform
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
