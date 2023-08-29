863bf8d6f74b69278db707c3d65f8e4b1d36f597
AlexanderStout <132599440+AlexanderStout@users.noreply.github.com>
2023-08-29 23:36:30 +0400

    kubectl

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# kubectl
+brew install kubectl
+
 # autocomplete for k8s
 source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
 echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
